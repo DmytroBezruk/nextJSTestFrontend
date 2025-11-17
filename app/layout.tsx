3c4cb1ee00c144fe5024a7d1762c8fc5b3f58a7d
Dmytro <dmytro.bezruk@planeks.net>
2025-11-17 17:17:57 +0200

    remove unused import; delete Metadata type import from layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@
 
 import { usePathname } from 'next/navigation'
 
-import type {Metadata} from "next";
 import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 import {Navbar} from "@/components/navbar";
